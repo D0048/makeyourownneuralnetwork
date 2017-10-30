b0aeeea4994ddd5abde1fad0c491e16198afaa42
d0048 <1198995013@qq.com>
2017-10-30 11:08:28 +0800

    Fail to implement x2
    ...looking into GAN implementations

--- better_detection/cnn_mnist_tfl.py
+++ better_detection/cnn_mnist_tfl.py
@@ -19,46 +19,44 @@ from tflearn.datasets import mnist
 
 
 def main(unused_argv):
-    global rec_model, old_graph, new_graph
-    rec_model = train_rec()
-
-    #old_graph = tf.get_default_graph()
-    #new_graph = tf.Graph()
-    #new_graph.as_default()
+    #global rec_model, old_graph, new_graph
+    #rec_model = train_rec()
 
     X, Y, X_test, Y_test = mnist.load_data(one_hot=True)
-
-    # Slice an image from batch
-    global x_origin, y_origin
-    x_origin, y_origin = X[1].reshape([-1, 28, 28, 1]), Y[1]
-    y_dummy = X[1]  #fake y that serves no purpose...
-    print("x origin: {}".format(x_origin.shape))
-    print("y origin: {}".format(y_origin.shape))
-    #exit()
 
     img_prep = ImagePreprocessing()
     img_aug = ImageAugmentation()
 
-    # Convolutional network building
-    new_network = input_data(
+    inputs = input_data(
         shape=[None, 28, 28, 1],
         data_preprocessing=img_prep,
         data_augmentation=img_aug)
+    # Classifier
+    classifier_fc1 = fully_connected(
+        inputs, 128, activation='relu', name="clssfr_fc1")
+    classifier_fc2 = fully_connected(
+        inputs, 128, activation='relu', name="clssfr_fc2")
+    classifier_fc3 = fully_connected(
+        inputs, 10, activation='relu', name="clssfr_fc3")
+    classifier_out = regression(
+        classifier_fc3, optimizer='adam', loss=new_cost, learning_rate=0.001)
+    print(classifier_out)
+    exit()
 
-    new_network = fully_connected(new_network, 128, activation='relu')
-    new_network = fully_connected(new_network, 128, activation='relu')
-    new_network = fully_connected(new_network, 28 * 28, activation='relu')
-    new_network = regression(
-        new_network, optimizer='adam', loss=new_cost, learning_rate=0.001)
+    # Confuser
+    confuser_fc1 = fully_connected(inputs, 128, activation='relu')
+    confuser_fc2 = fully_connected(confuser_fc1, 128, activation='relu')
+    confuser_fc3 = fully_connected(confuser_fc2, 28 * 28, activation='relu')
+    confuser_out = regression(
+        confuser_fc3, optimizer='adam', loss=new_cost, learning_rate=0.001)
+
     new_model = tflearn.DNN(new_network, tensorboard_verbose=0)
-
-    print(new_model.predict_label(x_origin.reshape(-1, 28, 28, 1)))
     new_model.fit(
-        x_origin,
-        y_dummy,
+        X,
+        Y,
         n_epoch=5,
         shuffle=False,
-        validation_set=(x_origin, y_dummy),
+        validation_set=(X_test, Y_test),
         show_metric=True,
         batch_size=1,
         run_id='mnist_counter')
@@ -67,23 +65,9 @@ def main(unused_argv):
 
 
 def new_cost(y_pred, y_true):
-    #x_origin: 28*28
-    #y_pred: 28*28mod
-    #y_true = y_origin: 1x10
-    #y_disturbed: evla(y_pred)
-    global x_origin, y_origin, rec_model, rec_input, rec_network, new_graph  #y_true is useless
-
     with tf.name_scope(None):
-        cost_from_similarity = tf.reduce_sum(
-            (y_pred - x_origin)**2)  #minimize (Y_pred-x_origin)^2
+        return tf.losses.sigmoid_cross_entropy(y_true, y_pred)
 
-        y_disturbed = rec_network  #↓  ts no len()
-
-        print(y_disturbed)
-        cost_from_distrubance = (
-            tf.reduce_sum(y_disturbed, y_origin)**2) * tf.Constant(-1)
-
-        return tf.reduce_sum(cost_from_similarity + cost_from_distrubance)
     pass
 
 
