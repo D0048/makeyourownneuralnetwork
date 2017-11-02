c60e517a08f0d9b787f22e08d81f57b455c167e9
d0048 <1198995013@qq.com>
2017-11-02 17:39:03 +0800

    Runnable
    TODO: display

--- better_detection/gan/gan.py
+++ better_detection/gan/gan.py
@@ -35,15 +35,6 @@ class MonitorCallback(tflearn.callbacks.Callback):
         pass
 
 
-# Data loading and preprocessing
-import tflearn.datasets.mnist as mnist
-X, Y, testX, testY = mnist.load_data()
-
-image_dim = 784  # 28*28 pixels
-z_dim = 200  # Noise data points
-total_samples = len(X)
-
-
 # Generator
 def generator(x, reuse=False):
     with tf.variable_scope('Generator', reuse=reuse):
@@ -56,27 +47,42 @@ def generator(x, reuse=False):
 def discriminator(x, reuse=False):
     with tf.variable_scope('Discriminator', reuse=reuse):
         x = tflearn.fully_connected(x, 256, activation='relu')
-        x = tflearn.fully_connected(x, 1, activation='sigmoid')
+        x = tflearn.fully_connected(x, 64, activation='relu')
+        x = tflearn.fully_connected(x, 10, activation='sigmoid')
         return x
 
 
+# Data loading and preprocessing
+import tflearn.datasets.mnist as mnist
+X, Y, testX, testY = mnist.load_data()
+image_dim = 784  # 28*28 pixels
+z_dim = 200  # Noise data points
+total_samples = len(X)
+
+Y = tf.one_hot(Y, 10,dtype=tf.float32)
+with tf.Session() as sess:
+    Y=sess.run(Y)
+    pass
+
 # Build Networks
-global z
-origin = X[1]
-z = origin.reshape([-1, 784])
-gen_input = tf.reshape(tf.constant(origin), [1, -1])
 #gen_input = tflearn.input_data(shape=[None, z_dim], name='input_noise')
+gen_input = tflearn.input_data(shape=[None, 784], name='disc_input')
 disc_input = tflearn.input_data(shape=[None, 784], name='disc_input')
+Y_feed = tflearn.input_data(shape=[None, 10], name='Y_feed')
 
 gen_sample = generator(gen_input)
 disc_real = discriminator(disc_input)
 disc_fake = discriminator(gen_sample, reuse=True)
 
-# Define Loss
+# Define Lossdisc_input
+disc_loss = tf.losses.sigmoid_cross_entropy(disc_real, Y_feed)
+gen_loss = tf.losses.sigmoid_cross_entropy(disc_fake, Y_feed)
+"""
 disc_loss = -tf.reduce_mean(tf.log(disc_real) + tf.log(1. - disc_fake))
 gen_loss = -tf.reduce_mean(
     tf.log(disc_fake)) + tf.constant(0.5) * tf.reduce_sum(
         (gen_input - generator(gen_input, reuse=True))**2)
+"""
 
 # Build Training Ops for both Generator and Discriminator.
 # Each network optimization should only update its own variable, thus we need
@@ -111,24 +117,13 @@ gan = tflearn.DNN(gen_model)
 z_dim = 784
 z = np.random.uniform(-1., 1., size=[total_samples, z_dim])
 # Start training, feed both noise and real images.
+
 gan.fit(
     X_inputs={  #gen_input: z,
-        disc_input: X
+        gen_input: X,
+        disc_input: X,
+        Y_feed: Y
     },
     Y_targets=None,
     n_epoch=50,
-    show_metric=True,
     callbacks=[MonitorCallback()])
-
-# Generate images from noise, using the generator network.
-f, a = plt.subplots(2, 10, figsize=(10, 4))
-for i in range(10):
-    for j in range(2):
-        # Noise input.
-        z = np.random.uniform(-1., 1., size=[1, z_dim])
-        # Generate image from noise. Extend to 3 channels for matplot figure.
-        temp = [[ii, ii, ii] for ii in list(gan.predict([z])[0])]
-        a[j][i].imshow(np.reshape(temp, (28, 28, 3)))
-f.show()
-plt.draw()
-input("")
