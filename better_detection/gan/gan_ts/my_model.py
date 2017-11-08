373051a831968fa2c002c89af933132fc33527dc
Unknown <1198995013@qq.com>
2017-11-08 13:32:01 +0800

    Done model/parameter save

--- better_detection/gan/gan_ts/my_model.py
+++ better_detection/gan/gan_ts/my_model.py
@@ -14,6 +14,7 @@ zdim = 200
 bs = 5
 lr_disc = 1e-5
 lr_gen = 1e-5
+log_dir = "/tmp/saved_models/"
 
 
 def main(*argv):
@@ -21,39 +22,42 @@ def main(*argv):
         tf.float32, shape=[bs, img_w, img_h, img_d])
     gen_input_rand = tf.placeholder(tf.float32, shape=[bs, zdim])
 
-    gened_disc_input_fake = generator(gen_input_rand)
-    disc_out_gened = discriminator(gened_disc_input_fake)
-    disc_out_real_max = discriminator(dis_input_real, reuse=True)
+    gened_fake = generator(gen_input_rand)
+    disc_fake = discriminator(gened_fake)
+    disc_real = discriminator(dis_input_real, reuse=True)
 
     #Losses
     #"""Classic
-    disc_loss = tf.log(tf.reduce_mean(disc_out_gened)) - tf.log(
-        tf.reduce_mean(disc_out_real_max))
-    gen_loss = -tf.log(tf.reduce_mean(disc_out_gened))
-    """
-    disc_loss = (tf.reduce_mean(disc_out_gened)
-                 )**2 - tf.reduce_mean(disc_out_real_max)**2
-    gen_loss = -(tf.reduce_mean(disc_out_gened))**2
-    """
+    disc_loss = -tf.reduce_mean(tf.log(disc_real) + tf.log(1. - disc_fake))
+    gen_loss = -tf.reduce_mean(tf.log(disc_fake))
 
     #Optimizers
     opt_disc_loss = tf.train.AdamOptimizer(lr_disc).minimize(disc_loss)
     opt_gen_loss = tf.train.AdamOptimizer(lr_gen).minimize(gen_loss)
 
+    #GPU Config
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.gpu_options.per_process_gpu_memory_fraction = 0.9
+
+    saver = tf.train.Saver()
+    train_writer = tf.summary.FileWriter(log_dir)
+    train_writer.add_graph(tf.get_default_graph())
+
     #Train
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
-    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+    with tf.Session(config=config) as sess:
         sess.run(tf.global_variables_initializer())
 
-        print(
+        print("Test Generation: {}".format(
             sess.run(
-                gened_disc_input_fake,
+                gened_fake,
                 feed_dict={
                     dis_input_real: next_batch(bs),
                     gen_input_rand: np.random.uniform(
                         -1., 1., size=[bs, zdim])
-                }))
-        for e_total in range(10):
+                })))
+
+        for e_total in range(100):
             #Discriminator train
             for e in range(5):
                 _, loss = sess.run(
@@ -64,8 +68,9 @@ def main(*argv):
                             -1., 1., size=[bs, zdim])
                     })
                 print("disc_loss: {}".format(loss))
-    
-                    #Generator train
+                pass
+
+                #Generator train
             for e in range(5):
                 _, loss = sess.run(
                     [opt_gen_loss, gen_loss],
@@ -75,6 +80,10 @@ def main(*argv):
                             -1., 1., size=[bs, zdim])
                     })
                 print("gen_loss: {}".format(loss))
+                pass
+
+            #Save Model
+            saver.save(sess, log_dir + "GAN_TF.ckpt")
             pass
         pass
     pass
@@ -104,7 +113,6 @@ def generator(x_rand, reuse=False):
             kernel_size=[1, 1],
             padding="same",
             activation=tf.nn.sigmoid)
-        #TODO: wrong output size
 
         print("Generator Output: {}".format(x_rand))
         return x_rand
@@ -161,8 +169,18 @@ data_as_array = np.asfarray(buf)
 
 def next_batch(bs):
     global bs_ptr, data_as_array
+    #IF OUT OF RANGE
+    data_end = data_as_array.shape[0]
+    if (data_as_array[bs_ptr:bs_ptr + bs, :, :, :].shape[0] < bs):#TODO
+        print("All data cycled!")
+        bs_ptr = 0
+        pass
     bs_ptr += bs
+    print('shape: {}\n bs_cmp: {}'.format(
+        data_as_array[bs_ptr:bs_ptr + bs, :, :, :].shape,
+        data_as_array[bs_ptr:bs_ptr + bs, :, :, :].shape[0] < bs))
     return data_as_array[bs_ptr:bs_ptr + bs, :, :, :]
+
     pass
 
 
