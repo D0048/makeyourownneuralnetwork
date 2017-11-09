81a82246dc2f77aef37cb0d6fe76c7c812cf84fe
Unknown <1198995013@qq.com>
2017-11-09 08:46:34 +0800

    Work on 1 channel

--- better_detection/gan/gan_ts/my_model.py
+++ better_detection/gan/gan_ts/my_model.py
@@ -2,22 +2,28 @@
 #-*- coding: utf-8 -*-
 from __future__ import division
 import os
+import sys
 import time
 import tensorflow as tf
 import tflearn
 import numpy as np
+import logger
+import matplotlib.pyplot as plt
 
-img_h = 64
-img_w = 64
-img_d = 3
+logger.bl_log = False
+img_h = 28
+img_w = 28
+img_d = 1
 zdim = 200
-bs = 5
-lr_disc = 1e-5
-lr_gen = 1e-5
+bs = 1
+lr_disc = 1e-2
+lr_gen = 1e-2
 log_dir = "/tmp/saved_models/"
 
 
 def main(*argv):
+    #imshow(next_batch(5))
+    #plt.waitforbuttonpress()
     dis_input_real = tf.placeholder(
         tf.float32, shape=[bs, img_w, img_h, img_d])
     gen_input_rand = tf.placeholder(tf.float32, shape=[bs, zdim])
@@ -32,13 +38,19 @@ def main(*argv):
     gen_loss = -tf.reduce_mean(tf.log(disc_fake))
 
     #Optimizers
-    opt_disc_loss = tf.train.AdamOptimizer(lr_disc).minimize(disc_loss)
-    opt_gen_loss = tf.train.AdamOptimizer(lr_gen).minimize(gen_loss)
+    trainable_gen = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                      "Generator/*")
+    trainable_disc = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                       "Discriminator/*")
+    opt_disc_loss = tf.train.GradientDescentOptimizer(lr_disc).minimize(
+        disc_loss, var_list=trainable_disc)
+    opt_gen_loss = tf.train.GradientDescentOptimizer(lr_gen).minimize(
+        gen_loss, var_list=trainable_gen)
 
     #GPU Config
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    config.gpu_options.per_process_gpu_memory_fraction = 0.9
+    config.gpu_options.per_process_gpu_memory_fraction = 0.6
 
     saver = tf.train.Saver()
     train_writer = tf.summary.FileWriter(log_dir)
@@ -48,44 +60,64 @@ def main(*argv):
     with tf.Session(config=config) as sess:
         sess.run(tf.global_variables_initializer())
 
-        print("Test Generation: {}".format(
-            sess.run(
+        imgs = []
+        for i in range(2):
+            img = sess.run(
                 gened_fake,
                 feed_dict={
                     dis_input_real: next_batch(bs),
                     gen_input_rand: np.random.uniform(
                         -1., 1., size=[bs, zdim])
-                })))
+                })
+            for im in img:
+                imgs.append(im)
+        imshow(imgs)
 
-        for e_total in range(100):
-            #Discriminator train
-            for e in range(5):
-                _, loss = sess.run(
-                    [opt_disc_loss, disc_loss],
-                    feed_dict={
-                        dis_input_real: next_batch(bs),
-                        gen_input_rand: np.random.uniform(
-                            -1., 1., size=[bs, zdim])
-                    })
-                print("disc_loss: {}".format(loss))
+        for e_total in range(26):
+            for e_both in range(80):
+                #Discriminator train
+                for e in range(4):
+                    _, loss = sess.run(
+                        [opt_disc_loss, disc_loss],
+                        feed_dict={
+                            dis_input_real:
+                            next_batch(bs),
+                            gen_input_rand:
+                            np.random.uniform(-1., 1., size=[bs, zdim])
+                        })
+                    print("disc_loss: {}".format(loss))
+                    pass
+
+                    #Generator train
+                for e in range(30):
+                    _, loss = sess.run(
+                        [opt_gen_loss, gen_loss],
+                        feed_dict={
+                            dis_input_real:
+                            next_batch(bs),
+                            gen_input_rand:
+                            np.random.uniform(-1., 1., size=[bs, zdim])
+                        })
+                    print("gen_loss: {}".format(loss))
+                    pass
                 pass
-
-                #Generator train
-            for e in range(5):
-                _, loss = sess.run(
-                    [opt_gen_loss, gen_loss],
-                    feed_dict={
-                        dis_input_real: next_batch(bs),
-                        gen_input_rand: np.random.uniform(
-                            -1., 1., size=[bs, zdim])
-                    })
-                print("gen_loss: {}".format(loss))
-                pass
-
+            pass
             #Save Model
             saver.save(sess, log_dir + "GAN_TF.ckpt")
+            #Visualize
+            imgs = []
+            for i in range(2):
+                img = sess.run(
+                    gened_fake,
+                    feed_dict={
+                        dis_input_real: next_batch(bs),
+                        gen_input_rand: np.random.uniform(
+                            -1., 1., size=[bs, zdim])
+                    })
+                for im in img:
+                    imgs.append(im)
+            if (e_total % 8 == 0): imshow(imgs)
             pass
-        pass
     pass
 
 
@@ -93,27 +125,35 @@ def main(*argv):
 def generator(x_rand, reuse=False):
     with tf.variable_scope("Generator", reuse=reuse):
         print("Generator Input: {}".format(x_rand))
-
+        """
         x_rand = tf.layers.dense(
             inputs=x_rand,
             units=(img_h / 4) * (img_w / 4) * img_d,
-            activation=tf.nn.relu)
+            activation=tf.nn.tanh)
 
         x_rand = tf.reshape(
             x_rand,
-            [-1, int(img_h / 8), int(img_w / 8), img_d])
-
-        #x_rand = tflearn.upsample_2d(x_rand, 2)
+            [-1, int(img_h / 4), int(img_w / 4), img_d])
         x_rand = tflearn.upsample_2d(x_rand, 2)
         x_rand = tflearn.upsample_2d(x_rand, 2)
+        """
+        x_rand = tf.layers.dense(
+            inputs=x_rand,
+            units=(img_h) * (img_w) * img_d,
+            activation=tf.nn.tanh)
 
+        x_rand = tf.reshape(x_rand, [-1, int(img_h), int(img_w), img_d])
+
+        x_rand = tf.contrib.layers.conv2d_transpose(
+            x_rand, 16, [8, 8], padding='SAME')
+        x_rand = tf.contrib.layers.conv2d_transpose(
+            x_rand, 16, [8, 8], padding='SAME')
         x_rand = tf.layers.conv2d(
             inputs=x_rand,
-            filters=3,
+            filters=img_d,
             kernel_size=[1, 1],
             padding="same",
             activation=tf.nn.sigmoid)
-
         print("Generator Output: {}".format(x_rand))
         return x_rand
     pass
@@ -123,31 +163,62 @@ def generator(x_rand, reuse=False):
 def discriminator(x_img, reuse=False):
     with tf.variable_scope("Discriminator", reuse=reuse):
         print("Discriminator Input: {}".format(x_img))
-        """
         x_img = tf.layers.conv2d(
             inputs=x_img,
-            filters=32,
-            kernel_size=[5, 5],
+            filters=64,
+            kernel_size=[8, 8],
             padding='same',
             activation=tf.nn.sigmoid)
-        """
         x_img = tf.layers.conv2d(
             inputs=x_img,
-            filters=6,
-            kernel_size=[3, 3],
+            filters=8,
+            kernel_size=[8, 8],
             padding='same',
-            activation=tf.nn.relu)
+            activation=tf.nn.sigmoid)
 
-        x_img = tf.reshape(x_img, [-1, 6 * img_h * img_w])
+        x_img = tf.reshape(x_img, [-1, 8 * img_h * img_w])
         #x_img = tf.reshape(x_img, [-1, img_d * img_h * img_w])
 
-        x_img = tf.layers.dense(inputs=x_img, units=128, activation=tf.nn.relu)
+        x_img = tf.layers.dense(
+            inputs=x_img, units=128, activation=tf.nn.sigmoid)
 
         x_img = tf.layers.dense(
             inputs=x_img, units=1, activation=tf.nn.sigmoid)
 
         print("Discriminator Output: {}".format(x_img))
         return x_img
+    pass
+
+
+def cli_imshow(img):
+    avg = np.average(img)
+    for i in img:
+        for j in i:
+            if (j.any() >= avg.any()):
+                sys.stdout.write('█')
+                pass
+            else:
+                sys.stdout.write(' ')
+            pass
+        print(" ")
+        pass
+    pass
+
+
+def imshow(imgs):
+    #f=plt.figure()
+    #plt.show(block = False)
+    f, a = plt.subplots(2, 10, figsize=(10, 4))
+    for i in range(imgs.__len__()):
+        j = 0
+        print(imgs[i].shape)
+        img = imgs[i].squeeze()
+        #img = imgs[i]#np.asfarray([imgs[i], imgs[i], imgs[i]]).reshape([28, 28, 3])
+        a[j][i].imshow(img, interpolation='nearest')
+        #a[j][i].imshow(imgs[i][:, :, :], interpolation='nearest')
+        pass
+    f.show()
+    plt.draw()
     pass
 
 
@@ -166,19 +237,26 @@ for img in data_as_array:
     pass
 data_as_array = np.asfarray(buf)
 
+#"""
+mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+train_data = mnist.train.images  # Returns np.array
+data_as_array = train_data.reshape([-1, 28, 28, 1])
+print(data_as_array.shape)
+imshow(data_as_array[1:5])
+#"""
 
 def next_batch(bs):
     global bs_ptr, data_as_array
     #IF OUT OF RANGE
     data_end = data_as_array.shape[0]
-    if (data_as_array[bs_ptr:bs_ptr + bs, :, :, :].shape[0] < bs):#TODO
-        print("All data cycled!")
+    bs_ptr += bs
+    if (data_as_array.shape[0] < (bs_ptr + bs)):
+        logger.btch_loader_log("All data cycled!")
         bs_ptr = 0
         pass
-    bs_ptr += bs
-    print('shape: {}\n bs_cmp: {}'.format(
+    logger.btch_loader_log('shape: {}| bs_cmp: {}'.format(
         data_as_array[bs_ptr:bs_ptr + bs, :, :, :].shape,
-        data_as_array[bs_ptr:bs_ptr + bs, :, :, :].shape[0] < bs))
+        data_as_array.shape[0] < (bs_ptr + bs)))
     return data_as_array[bs_ptr:bs_ptr + bs, :, :, :]
 
     pass
